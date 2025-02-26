d3b500fdad782694db0e61f797e57045b381a594
av_suhov <av_suhov@komus.net>
2025-02-26 17:00:00 +0300

    ДЗ №3. "Чёрная магия" Spring Boot

--- hw03/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw03/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,15 +1,19 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
-import java.util.ArrayList;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
+@Repository
 @RequiredArgsConstructor
-@Component
 public class CsvQuestionDao implements QuestionDao {
     private final TestFileNameProvider fileNameProvider;
 
@@ -20,6 +24,17 @@ public class CsvQuestionDao implements QuestionDao {
         // Использовать QuestionReadException
         // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
 
-        return new ArrayList<>();
+        try {
+            InputStream is = getClass().getClassLoader().getResourceAsStream(fileNameProvider.getTestFileName());
+            List<QuestionDto> questions = new CsvToBeanBuilder<QuestionDto>(new InputStreamReader(is))
+                .withSkipLines(1)
+                .withSeparator(';')
+                .withType(QuestionDto.class)
+                .build()
+                .parse();
+            return questions.stream().map(QuestionDto::toDomainObject).toList();
+        } catch (Exception e) {
+            throw new QuestionReadException("Error reading file with questions", e);
+        }
     }
 }
